13099a31ca79edc9822365c0e56ce8fbc812c8b0
popandepo <popandepo@gmail.com>
2022-08-03 13:34:35 +0200

    i don't know

---
+++ ImplicitOperatorTest/LongNumber.cs
@@ -0,0 +1,116 @@
+﻿namespace ImplicitOperatorTest;
+
+public class LongNumber
+{
+	public string Value;
+
+	public LongNumber(string value)
+	{
+		Value = value;
+	}
+
+	public static implicit operator LongNumber(string number)
+	{
+		return new LongNumber(number);
+	}
+
+	public static implicit operator LongNumber(int number)
+	{
+		return new LongNumber(number.ToString());
+	}
+
+	public static LongNumber operator +(LongNumber ln1, LongNumber ln2)
+	{
+		string result = ln1.Add(ln2).Value;
+		return result;
+	}
+
+	public (string top, string bottom) Pad(string top, string bottom)
+	{
+		if (top.Length < bottom.Length)
+			top = top.PadLeft(bottom.Length, '0');
+		else if (bottom.Length < top.Length) bottom = bottom.PadLeft(top.Length, '0');
+
+		return (top, bottom);
+	}
+
+	public LongNumber Add(LongNumber ln)
+	{
+		var remainder = 0;
+		string top = Value;
+		string bottom = ln.Value;
+		var result = "";
+
+		var padded = Pad(top, bottom);
+		top = padded.top;
+		bottom = padded.bottom;
+
+		for (int i = Math.Max(top.Length - 1, bottom.Length - 1); i >= 0; i--)
+		{
+			int topDigit = ParseStringAsInt(top, i);
+			int bottomDigit = ParseStringAsInt(bottom, i);
+
+			int sum = AddDigits(topDigit, bottomDigit, remainder);
+
+			remainder = GetRemainder(sum);
+			result = result.Insert(0, char.ToString(sum.ToString().LastOrDefault()));
+		}
+
+		Value = remainder > 0 ? result.Insert(0, remainder.ToString()) : result;
+		return this;
+	}
+
+	public int GetRemainder(int sum)
+	{
+		return sum > 9 ? ParseStringAsInt(sum.ToString(), 0) : 0;
+	}
+
+	public static int ParseStringAsInt(string str, int position)
+	{
+		return int.Parse(char.ToString(str.ElementAtOrDefault(position)));
+	}
+
+	public int AddDigits(int topDigit, int bottomDigit, int remainder)
+	{
+		return topDigit + bottomDigit + remainder;
+	}
+
+	public static LongNumber operator -(LongNumber ln1, LongNumber ln2)
+	{
+		return ln1.Subtract(ln2);
+	}
+
+	public LongNumber Subtract(LongNumber ln2)
+	{
+		var remainder = 0;
+		string top = Value;
+		string bottom = ln2.Value;
+		var result = "";
+
+		var padded = Pad(top, bottom);
+		top = padded.top;
+		bottom = padded.bottom;
+
+		for (int i = Math.Max(top.Length - 1, bottom.Length - 1); i >= 0; i--)
+		{
+			int topDigit = int.Parse(char.ToString(top.ElementAtOrDefault(i)));
+			int bottomDigit = int.Parse(char.ToString(bottom.ElementAtOrDefault(i)));
+			int subtracted = topDigit - bottomDigit - remainder;
+			remainder = subtracted < 0 ? 1 : 0;
+			result = result.Insert(0, char.ToString(subtracted.ToString().ElementAtOrDefault(1)));
+		}
+
+		return result;
+	}
+
+	// public override string ToString()
+	// {
+	// 	//for readability, add a space every 3 digits from the right
+	// 	string result = Value;
+	// 	for (int i = result.Length - 3; i > 0; i -= 3)
+	// 	{
+	// 		result = result.Insert(i, " ");
+	// 	}
+	// 	return result;
+	// }
+}
--- ImplicitOperatorTest/Program.cs
+++ ImplicitOperatorTest/Program.cs
@@ -1,75 +1,12 @@
 ﻿namespace ImplicitOperatorTest;
 
-public class Program
+public partial class Program
 {
 	public static void Main(string[] args)
 	{
-		LongNumber ln = "999999999999999999999999999999999999999999999999999999999999999999999999999";
+		LongNumber ln = 5;
 		int one = 1;
 		LongNumber ln2 = ln + one;
-	}
-
-	public class LongNumber
-	{
-		public string Number;
-
-		public LongNumber(string number)
-		{
-			Number = number;
-		}
-
-		public static implicit operator LongNumber(string number)
-		{
-			return new LongNumber(number);
-		}
-
-		public static implicit operator LongNumber(int number)
-		{
-			return new LongNumber(number.ToString());
-		}
-
-		public static LongNumber operator +(LongNumber ln1, LongNumber ln2)
-		{
-			return ln1.Add(ln2);
-		}
-
-		private LongNumber Add(LongNumber ln)
-		{
-			int remainder = 0;
-			string top = Number;
-			string bottom = ln.Number;
-			string result = "";
-			
-			// if the numbers are of different lengths, pad the shorter one with zeros
-			if (top.Length < bottom.Length)
-			{
-				top = top.PadLeft(bottom.Length, '0');
-			}
-			else if (bottom.Length < top.Length)
-			{
-				bottom = bottom.PadLeft(top.Length, '0');
-			}
-			
-			for (int i = Math.Max(top.Length - 1, bottom.Length - 1); i >= 0; i--)
-			{
-				int topDigit = int.Parse(char.ToString(top.ElementAtOrDefault(i)));
-				int bottomDigit = int.Parse(char.ToString(bottom.ElementAtOrDefault(i)));
-				int added = topDigit + bottomDigit + remainder;
-				remainder = added > 9 ? int.Parse(char.ToString(added.ToString().ElementAtOrDefault(0))) : 0;
-				result = result.Insert(0, char.ToString(added.ToString().ElementAtOrDefault(1)));
-			}
-			return remainder > 0 ? new LongNumber(result.Insert(0, remainder.ToString())) : new LongNumber(result);
-		}
-
-		public override string ToString()
-		{
-			//for readability, add a space every 3 digits from the right
-			string result = Number;
-			for (int i = result.Length - 3; i > 0; i -= 3)
-			{
-				result = result.Insert(i, " ");
-			}
-			return result;
-		}
+		LongNumber ln3 = ln - "1";
 	}
 }
